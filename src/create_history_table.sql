bffc3378f8b8edc018c3b3573f16da24c6e6f6d9
Chuya Goto <pillochu5@egmail.com>
2020-04-15 00:56:50 +0900

    Delete old version files.

--- src/create_history_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS othelloHistory;
-CREATE TABLE IF NOT EXISTS othelloHistory (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    firstUserId INT,
-    passiveUserId INT,
-    winUserId INT 
-);
-
-
